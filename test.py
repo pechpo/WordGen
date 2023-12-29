94ebf8de55c53b8b16b23a2ca41cb698c6c2eebe
pechpo <pechpo@qq.com>
2023-12-30 05:06:21 +0800

    update

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from model import *
 from dataset import *
 from tqdm import tqdm
 from setting import *
-import torch.optim as optim
+import pandas as pd
 
 # 数据集处理
 batch_size = 10
@@ -19,7 +19,6 @@ hidden_dim = 512  # 隐藏层大小
 num_layers = 6  # Transformer层数
 num_heads = 8  # 注意力头数
 dropout = 0.1  # Dropout概率
-epoch = 10
 
 encoder = TransformerEncoder(
     input_dim, hidden_dim, num_layers, num_heads, dropout)
@@ -30,9 +29,9 @@ model = model.to(device)
 model.load_state_dict(torch.load("model.pth"))
 
 model.eval()
-res = [""] * len(test_dataset)
+res = [[]] * len(test_dataset)
 with torch.no_grad():
-    for seq_in, seq_out, mask_in, mask_out in tqdm(test_dataloader):
+    for batch_id, seq_in, seq_out, mask_in, mask_out in tqdm(enumerate(test_dataloader)):
         seq_in = seq_in.to(device)
         seq_out = seq_out.to(device)
         mask_in = mask_in.to(device)
@@ -40,6 +39,26 @@ with torch.no_grad():
         mask_out = None
         # 测试的阶段使用串行的token生成
         decoder_input = torch.tensor(seq_out).to(device)
-        for i in range(1, seq_out.shape(1)):
+        token_pos = 1
+        not_end = [False] * batch_size  # 表示该条故事是否还在生成
+        while any(not_end):
             output = model(seq_in, decoder_input[:, :-1], mask_in, mask_out)
-            decoder_input[:, i] = torch.argmax(output)
+            decoder_input[:, token_pos] = torch.argmax(output, dim=2)
+            for i in range(0, batch_size):
+                if not_end[i] == False:
+                    continue
+                id = batch_id * batch_size + i
+                res[i].append(decoder_input[i][token_pos])
+                if res[i] == end_token:
+                    not_end[i] = False
+            token_pos += 1
+
+enc = tiktoken.get_encoding("cl100k_base")
+id_list = []
+text_result = []
+for story_id, seq in enumerate(res):
+    text = enc.decode(seq)
+    id_list.append(story_id)
+    text_result.append(text)
+table = pd.DataFrame({"story id": id_list, "text": text_result})
+table.to_csv("result.csv")
