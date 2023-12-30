7c8178106b81981f07db3836216bbc9189d693c5
pechpo <pechpo@qq.com>
2023-12-31 03:26:08 +0800

    update

---
+++ model3/dataset.py
@@ -0,0 +1,28 @@
+from datasets import load_dataset
+import pandas as pd
+from transformers import AutoTokenizer
+from transformers import DataCollatorForLanguageModeling
+
+# 获得数据集
+path_train = "../story_generation_dataset/ROCStories_train.csv"
+path_val = "../story_generation_dataset/ROCStories_val.csv"
+path_test = "../story_generation_dataset/ROCStories_test.csv"
+data_files = {"train": path_train, "val": path_val, "test":path_test}
+
+datasets = load_dataset("csv", data_files=data_files)
+
+# 处理数据集
+tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
+
+def preprocess_function(item):
+    seq = item["storytitle"] + "\n"
+    for i in range(1, 6):
+        seq += item["sentence" + str(i)] + " "
+    return tokenizer([seq])
+
+tokenized_datasets = datasets.map(preprocess_function)
+
+#print(tokenized_datasets["train"][0])
+
+tokenizer.pad_token = tokenizer.eos_token
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
---
+++ model3/main.py
@@ -0,0 +1,36 @@
+from dataset import *
+from transformers import AutoModelForCausalLM, TrainingArguments, Trainer
+import math
+from transformers import pipeline
+
+#获取模型
+model = AutoModelForCausalLM.from_pretrained("distilgpt2")
+
+#训练
+training_args = TrainingArguments(
+    output_dir="my_awesome_eli5_clm-model",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    weight_decay=0.01,
+    push_to_hub=True,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["val"],
+    data_collator=data_collator,
+)
+
+trainer.train()
+
+#评估模型
+eval_results = trainer.evaluate()
+print(f"Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
+
+#测试模型
+
+prompt = "Bad Dream\nTommy was very close to his dad and loved him greatly. His was a cop and was shot and killed on duty. "
+generator = pipeline("text-generation", model="my-gpt2-model")
+generator(prompt)
